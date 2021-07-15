88932bf4b19a1ec2164424491c3d10799f41bba2
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2021-07-15 13:37:12 +0530

    creates necessary components, service, model

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDosComponent } from './components/to-dos/to-dos.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { HeaderComponent } from './layouts/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDosComponent,
+    TodoFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ todo-app/src/app/models/ToDo.ts
@@ -0,0 +1,6 @@
+export interface ToDo {
+    id: string;
+    title: string;
+    completed: boolean;
+    dueDate: Date;
+}
